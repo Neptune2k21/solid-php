d4ab3a3f98db5903a9f065e05c367c88e0bc9e89
Neptune2k21 <116343729+Neptune2k21@users.noreply.github.com>
2024-12-11 11:47:39 +0100

    I as Interface Segregation Principle

---
+++ exo-solid-4/PushEvent.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once 'EventInterface.php';
+
+class PushEvent implements EventInterface
+{
+    public function name() : string
+    {
+        return 'PushEvent';
+    }
+
+    public function fields() : array
+    {
+        return [
+            'name' => $this->name,
+            'priority' => 999,
+        ];
+    }
+
+    public function payload() : array
+    {
+        return [
+            
+            'cost' => 0,
+            'from' => 'https://github.com/api/v3/push',
+        ];
+    }
+}
